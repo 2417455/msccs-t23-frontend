38efdf070dad84597e50eb64f98f77343eff48c5
2417455 <2417455D@student.gla.ac.uk>
2018-10-31 07:07:06 -0700

    added mongodb

--- index.js
+++ index.js
@@ -6,4 +6,20 @@ app.get("/", (req, res) => {
     res.send({ hello: "world test" });
 });
 
-app.listen(PORT);
+
+var MongoClient = require('mongodb').MongoClient
+
+MongoClient.connect('mongodb+srv://Admin_1:glasgowcom@cluster0-0yvu9.gcp.mongodb.net/test?retryWrites=true', { useNewUrlParser: true }, function (err, client) {
+  if (err) throw err
+
+  var db = client.db('Helpme')
+  
+  db.collection('Tweets').find().toArray(function (err, result) {
+    if (err) throw err
+
+    console.log(result)
+  })
+})
+
+
+app.listen(PORT || 3000);
